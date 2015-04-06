091da69d2550dae6b3382e52dadd6483770cafcd
greenteamer <greenteamer@bk.ru>
2015-04-06 19:44:12 +0600

    разобрался с настройками вроде))

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'project.core',
+    'ckeditor',
+    'mptt',
+    'mptt_tree_editor'
 )
 
 MIDDLEWARE_CLASSES = (
