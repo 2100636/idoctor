9b29cc7386ba573288bfd527db02714036df912d
greenteamer <greenteamer@bk.ru>
2015-04-08 17:43:09 +0600

    cropping bugfix

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from easy_thumbnails.conf import Settings as thumbnail_settings
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -138,6 +139,10 @@ MEDIA_URL = '/media/'
 CKEDITOR_UPLOAD_PATH = '/media/uploads'
 
 THUMBNAIL_DEBUG = True
+
+THUMBNAIL_PROCESSORS = (
+    'image_cropping.thumbnail_processors.crop_corners',
+) + thumbnail_settings.THUMBNAIL_PROCESSORS
 
 try:
     from settings_local import *
